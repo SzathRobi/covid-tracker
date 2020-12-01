42683053c6b439fb910e99bc8c1c953428154c94
DevVatu <devvatu@gmail.com>
2020-12-01 09:30:34 +0100

    Updates

---
+++ precache-manifest.fc1ba70cbd2645ab0d95003087a43415.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf425bdcc1859e7968ece817bb1015cb",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "52255b3911abcedc81b1",
+    "url": "/covid-tracker/static/css/2.a4567992.chunk.css"
+  },
+  {
+    "revision": "111128b9c4fe67afd51b",
+    "url": "/covid-tracker/static/css/main.51aa3cba.chunk.css"
+  },
+  {
+    "revision": "52255b3911abcedc81b1",
+    "url": "/covid-tracker/static/js/2.8c0c71ff.chunk.js"
+  },
+  {
+    "revision": "94f0ce8a54864ea7af667721b3bf9fcc",
+    "url": "/covid-tracker/static/js/2.8c0c71ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "111128b9c4fe67afd51b",
+    "url": "/covid-tracker/static/js/main.227eecc1.chunk.js"
+  },
+  {
+    "revision": "b870be8bf331379ef191",
+    "url": "/covid-tracker/static/js/runtime-main.a224a4ef.js"
+  }
+]);
